d8d6432a1f87c4c674a306ffb4e33580f1b74382
someone250 <ktat@caltech.edu>
2017-04-01 02:27:22 -0700

    Parsed data for 10 specific features

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import csv
 import numpy as np
+import math
 
 train = 'data/planets.csv'
 
 # preprocess data
+
 
 def get_data(filename):
     with open(filename, 'r') as data_file:
@@ -12,13 +14,40 @@ def get_data(filename):
     raw_data = raw_data[74:]
     data_array = np.asarray(raw_data, dtype=str)
 
-    columns = [4,5,9,13,17,21,26,30,54,59,64]
-    final = data_array[:,0]
+    columns = [4, 5, 9, 17, 21, 26, 30, 54, 59, 64]
+    final = data_array[:, 0]
 
     for i in xrange(len(columns)):
-        final = np.c_[final, data_array[:,columns[i]]]
+        final = np.c_[final, data_array[:, columns[i]]]
 
-    print final
+    t1 = np.nan
+    t2 = str(t1)
+
+    final[final == ''] = t2
+
+    final = final.astype(float)
+
+    means = []
+
+    for j in xrange(11):
+        mean = np.nanmean(final[:, j])
+        means.append(mean)
+
+    # print means
+
+    reducedlist = []
+    for k in xrange(3472):
+        has_nan = False
+        for j in xrange(len(final[k])):
+            if math.isnan(final[k][j]):
+                has_nan = True
+        if not has_nan:
+            reducedlist.append(final[k])
+
+    print "this is reducued list: " + str(len(reducedlist))
+
+
     return final
+
 
 get_data(train)
