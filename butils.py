f74aea673fa941b8eab380fea1969c5e54d7c2f1
someone250 <ktat@caltech.edu>
2017-04-01 12:25:30 -0700

    Computed ESI for each planet

--- butils.py
+++ butils.py
@@ -4,9 +4,8 @@ import math
 
 train = 'data/cumulative.csv'
 
+
 # preprocess data
-
-
 def get_data(filename):
     # extract data from cumulative.csv file
     with open(filename, 'r') as data_file:
@@ -35,9 +34,33 @@ def get_data(filename):
     return means, final
 
 
+# calculate earth similarity index of planets for labelling
+def ESI(data):
+    e_flux = 1.3615  # kW/m^2
+    e_radius = 6371  # km
+
+    p_radii = data[:, 1] * e_radius
+    p_fluxes = data[:, 3] * e_flux
+
+    flux_top = p_fluxes - e_flux
+    flux_bottom = p_fluxes + e_flux
+
+    flux = (flux_top / flux_bottom) ** 2
+
+    radii_top = p_radii - e_radius
+    radii_bottom = p_radii + e_radius
+
+    radii = (radii_top / radii_bottom) ** 2
+
+    esi = 1 - np.sqrt(0.5 * (flux + radii))
+
+    return esi.reshape((len(esi), 1))
+
+
 def main():
     train = 'data/cumulative.csv'
     means, final = get_data(train)
+
 
 
 main()
