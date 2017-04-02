5246c08a398c4ef42aa89a22e2a8293fd8cdb330
someone250 <ktat@caltech.edu>
2017-04-02 07:38:52 -0700

    Django

--- application.py
+++ application.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-	#return redirect("http://cool.us-west-2.elasticbeanstalk.com/index.html", code=302)
+#!/usr/bin/env python
 
 if __name__ == "__main__":
-	app.run()
+    return redirect("http://cool.us-west-2.elasticbeanstalk.com/index.html", code="302")
